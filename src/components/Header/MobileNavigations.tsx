27e3a6f7d574c2a1792d5cf61addd9f229e9de23
Syed <nomaanulhasan+github@gmail.com>
2023-08-27 02:46:40 +0530

    mobile nav width - full screen

--- src/components/Header/MobileNavigations.tsx
+++ src/components/Header/MobileNavigations.tsx
@@ -10,18 +10,18 @@ export default function MobileNavigations() {
   const [isClosed, setIsClosed] = useState(true);
   const toggleMenu = () => setIsOpen((state) => !state);
   useEffect(() => {
-  if (isOpen) {
-    setIsClosed(false)
-    return
-  }
-  
-  const timer = setTimeout(() => {
-    setIsClosed(true)
-  },300)
+    if (isOpen) {
+      setIsClosed(false);
+      return;
+    }
 
-  return () => clearTimeout(timer)
-  },[isOpen]);
-  
+    const timer = setTimeout(() => {
+      setIsClosed(true);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [isOpen]);
+
   return (
     <div className="relative overflow-hidden">
       <button
@@ -32,14 +32,14 @@ export default function MobileNavigations() {
       >
         <div
           className="
-						h-[2px] w-[24px] origin-bottom-left bg-main-blue transition
+						bg-main-blue h-[2px] w-[24px] origin-bottom-left transition
             group-[.menuOpen]:-translate-y-[1px] group-[.menuOpen]:translate-x-[3px] group-[.menuOpen]:rotate-45
 					"
         />
-        <div className="h-[2px] w-[24px] bg-main-blue transition group-[.menuOpen]:invisible" />
+        <div className="bg-main-blue h-[2px] w-[24px] transition group-[.menuOpen]:invisible" />
         <div
           className="
-						h-[2px] w-[24px] origin-bottom-left bg-main-blue transition
+						bg-main-blue h-[2px] w-[24px] origin-bottom-left transition
             group-[.menuOpen]:ml-[2px] group-[.menuOpen]:-translate-y-[2px] group-[.menuOpen]:translate-x-[3px] group-[.menuOpen]:-rotate-45
 					"
         />
@@ -47,8 +47,10 @@ export default function MobileNavigations() {
 
       <nav
         className={`
-          ${isClosed ? 'absolute' : 'fixed'} left-0 top-[116px] flex w-[calc(100vw-6px)] flex-col items-center justify-center
-          gap-2 bg-main-blue/80 px-6 py-4 backdrop-blur-sm transition duration-300 ${
+          ${
+            isClosed ? "absolute" : "fixed"
+          } bg-main-blue/80 left-0 top-[116px] flex w-screen flex-col items-center
+          justify-center gap-2 px-6 py-4 backdrop-blur-sm transition duration-300 ${
             isOpen ? "translate-x-0" : "translate-x-[101vw]"
           }
         `}
@@ -58,8 +60,8 @@ export default function MobileNavigations() {
             key={id}
             href={url}
             className="
-              w-full whitespace-nowrap border-b border-white/10 px-2 py-4 text-center font-semibold
-              text-white transition last:border-b-0 hover:text-nav-hover xl:px-4
+              hover:text-nav-hover w-full whitespace-nowrap border-b border-white/10 px-2 py-4 text-center
+              font-semibold text-white transition last:border-b-0 xl:px-4
             "
           >
             {label}
