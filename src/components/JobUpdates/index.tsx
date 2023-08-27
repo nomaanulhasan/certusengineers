15b2bd5d03418f2e9ae2f547461022ce9181ea6c
Syed <nomaanulhasan+github@gmail.com>
2023-08-27 05:49:10 +0530

    Jobs section content added

--- src/components/JobUpdates/index.tsx
+++ src/components/JobUpdates/index.tsx
@@ -1,3 +1,139 @@
+import { faCalendarDays, faTag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
+
+const JOB_UPDATES_POSTS = [
+  {
+    id: 1,
+    title: "Audit /AccountAssistant",
+    desc: "Audit /AccountAssistant Eligibility Criteria Having Commerce Background Computer knowledge Experience Preferred…",
+    post_pic: "/images/tesimg.jpg",
+    publish_date: "Aug 27, 2023",
+    post_type: "Jobs",
+    author: {
+      name: "Certus Engineers",
+      profile_url: "/author",
+      profile_pic: "/images/user.jpeg",
+    },
+  },
+  {
+    id: 2,
+    title: "DRAFTMAN",
+    desc: "Eligiblity MSC COMPUTER/GEOLOGYDIPLOMA/B.E. IN CIVIL ENGG /CIVIL CONTACT : Mrs. ANKITA…",
+    post_pic: "/images/Draftsman-Job-Description.jpeg",
+    publish_date: "Aug 26, 2023",
+    post_type: "Jobs",
+    author: {
+      name: "Certus Engineers",
+      profile_url: "/author",
+      profile_pic: "/images/user.jpeg",
+    },
+  },
+  {
+    id: 3,
+    title: "Office work",
+    desc: "Require Candidatefor full Time BackOffice Workhaving goodcommand inEnglish n alsoComputerKnowledge Contact…",
+    post_pic: "/images/customer-service-call-center.jpg",
+    publish_date: "Aug 25, 2023",
+    post_type: "Jobs",
+    author: {
+      name: "Certus Engineers",
+      profile_url: "/author",
+      profile_pic: "/images/user.jpeg",
+    },
+  },
+];
+
 export default function JobUpdates() {
-  return <div>JobUpdates</div>;
+  return (
+    <div className="flex justify-center p-10 px-5 sm:p-20">
+      <div className="max-w-tv w-full">
+        <h4 className="text-section-head text-center font-semibold uppercase leading-loose">
+          Recent Articles
+        </h4>
+        <h3 className="text-primary-text text-center text-2xl font-medium leading-loose md:text-3xl">
+          Job Updates
+        </h3>
+
+        <div className="mt-8 grid w-full gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {JOB_UPDATES_POSTS.map(
+            ({
+              id,
+              post_pic,
+              title,
+              desc,
+              publish_date,
+              post_type,
+              author: { name, profile_url, profile_pic },
+            }) => (
+              <div
+                key={id}
+                className="shadow-job-card overflow-hidden rounded-md"
+              >
+                <div className="relative flex h-60 items-center justify-center overflow-hidden">
+                  <Image
+                    src={post_pic}
+                    alt={title}
+                    width={350}
+                    height={230}
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+                <div>
+                  <div className="flex flex-1 flex-col gap-4 p-6">
+                    <div className="text-secondary-text flex gap-4 text-sm">
+                      <div className="flex items-center justify-center gap-2">
+                        <FontAwesomeIcon
+                          icon={faCalendarDays}
+                          className="text-section-head text-base"
+                        />
+                        {publish_date}
+                      </div>
+                      <div className="flex items-center justify-center gap-2">
+                        <FontAwesomeIcon
+                          icon={faTag}
+                          className="text-section-head text-base"
+                        />
+                        {post_type}
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <Link
+                        href={`/post/${id}`}
+                        className="text-main-orange hover:text-main-blue text-xl font-semibold"
+                      >
+                        {title}
+                      </Link>
+                      <div className="text-secondary-text text-lg leading-relaxed">
+                        {desc}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-4 border-t border-[#e3e3e3] p-6 text-sm font-medium">
+                    <Image
+                      src={profile_pic}
+                      className="rounded-full"
+                      alt={name}
+                      width={35}
+                      height={35}
+                    />
+                    <div>
+                      By{" "}
+                      <Link
+                        href={profile_url}
+                        className="text-main-orange hover:text-main-blue"
+                      >
+                        {name}
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,29 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        'top-strip': '#e5eef6',
-        'card-border': '#e6ecf7',
-        'main-blue': '#022950',
-        'main-orange': '#ff6d00',
-        'primary-text': '#1d2c38',
-        'nav-hover': '#0093d8',
-        'secondary-text': '#454545',
-        'section-head': '#104cba',
-        'card-head': '#1575A3',
-        'footer-separator': '#223452',
+        "top-strip": "#e5eef6",
+        "card-border": "#e6ecf7",
+        "main-blue": "#022950",
+        "main-orange": "#ff6d00",
+        "primary-text": "#1d2c38",
+        "nav-hover": "#0093d8",
+        "secondary-text": "#454545",
+        "section-head": "#104cba",
+        "card-head": "#1575A3",
+        "footer-separator": "#223452",
       },
       spacing: {
-        'top-strip': '52px',
+        "top-strip": "52px",
       },
       maxWidth: {
-        tv: '1536px',
+        tv: "1536px",
+      },
+      boxShadow: {
+        "job-card": "0px 0px 30px 0px hsl(0deg 0% 0% / 7%)",
       },
     },
   },
