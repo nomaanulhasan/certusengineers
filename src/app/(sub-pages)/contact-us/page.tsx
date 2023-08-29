1508f756cf546668bc460d766d2b466c0036b52a
Syed <nomaanulhasan+github@gmail.com>
2023-08-29 12:43:27 +0530

    contact page title updates

--- src/app/(sub-pages)/contact-us/page.tsx
+++ src/app/(sub-pages)/contact-us/page.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import { PageWrapper, Container } from "@/components";
 
 export const metadata: Metadata = {
-  title: "Contact - Certus Engineers",
+  title: "Contact Us - Certus Engineers",
 };
 
 export default function Conatct() {
   return (
-    <PageWrapper title="Contact">
+    <PageWrapper title="Contact Us">
       <Container>Conatct Us Page Content</Container>
     </PageWrapper>
   );
