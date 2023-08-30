fefc38eed02385463925f186a7844f0f0691050d
Syed <nomaanulhasan+github@gmail.com>
2023-08-30 16:05:44 +0530

    Get In Touch: center head text

--- src/components/GetInTouch/index.tsx
+++ src/components/GetInTouch/index.tsx
@@ -6,12 +6,12 @@ export default function GetInTouch() {
   return (
     <Container>
       <div className="my-12 max-w-tv pb-8">
-        <div className="shadow-contact-card rounded-md px-4 py-6 lg:p-8">
+        <div className="rounded-md px-4 py-6 shadow-contact-card lg:p-8">
           <div className="flex flex-col gap-8 lg:flex-row">
             <div className="flex-1">
               <h1
                 className="
-                  mb-4 px-6 text-2xl font-medium text-primary-text lg:px-0 lg:text-3xl lg:leading-normal
+                  mb-4 px-6 text-center text-2xl font-medium text-primary-text sm:text-left lg:px-0 lg:text-3xl lg:leading-normal
                 "
               >
                 Get In Touch
